37f1a699b01a8fe88a4de8b387004e505a66e0bc
Your Name <user@example.com>
2025-10-23 19:54:27 +1000

    Remove About and Contact menu items from navigation

--- app/Views/public_template.php
+++ app/Views/public_template.php
@@ -237,12 +237,6 @@
                     <li class="nav-item">
                         <a class="nav-link" href="<?= base_url() ?>">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= base_url('about') ?>">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= base_url('contact') ?>">Contact</a>
-                    </li>
                 </ul>
             </div>
         </div>
